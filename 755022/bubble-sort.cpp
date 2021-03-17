6b7d11cc0d135dcaa56965bfbe5744b95717422a
JimmyNH3 <jimmy20060504@gmail.com>
2021-03-17 23:08:36 +0800

    20210311

---
+++ 755022/bubble-sort.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    int bobble[5] = {5, 2, 1, 4, 3};
+    int a;
+    
+    for (int j = 0; j < 5; j++) {
+        for (int i = 0; i < 5; i++) {
+            if (bobble[i] > bobble[i + 1]) {
+                a = bobble[i];
+                bobble[i] = bobble[i + 1];
+                bobble[i + 1] = a;
+            }
+        }
+    }
+
+    for (int i = 0; i < 5; i++) {
+        cout << bobble[i] << ",";
+    }
+}
