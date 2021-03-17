fe624d4adb4aab4e57aa07b9d3022122a088657f
林柏崴 <wayne20051211@gmail.com>
2021-03-17 20:43:46 +0800

    homework_submit

---
+++ 755446/bubble_sort.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main() {
+    int arr[5] = {};
+    int tmp;
+    srand(time(0));
+    for (int i = 0; i < 5; i++) {
+        arr[i] = rand() % 1000;
+    }
+    cout << "排序前: ";
+    for (int i = 0; i < 5; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    for (int i = 5; i > 0; i--) {
+        for (int j = 0; j < i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                tmp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tmp;   
+            }
+        }
+    }
+    cout << "排序後: ";
+    for (int i = 0; i < 5; i++) {
+        cout << arr[i] << " ";
+    }
+}
---
+++ 755446/insertion_sort.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main() {
+    int arr[5] = {};
+    int tmp;
+    srand(time(0));
+    for (int i = 0; i < 5; i++) {
+        arr[i] = rand() % 1000;
+    }
+    cout << "排序前: ";
+    for (int i = 0; i < 5; i++) {
+        cout << arr[i] << " ";
+    }
+
+    for (int i = 0; i < 5; i++) {
+        for (int j = i; j > 0; j--) {
+            if (arr[j] < arr[j - 1]){
+            tmp = arr[j - 1];
+            arr[j - 1] = arr[j];
+            arr[j] = tmp;
+            }
+        }
+    }
+    cout << "排序後: ";
+    for (int i = 0; i < 5; i++) {
+        cout << arr[i] << " ";
+    }
+}
