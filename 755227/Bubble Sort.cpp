af900293852f13ba5b5ad014ed4e96cec9bbee34
jifong <70698452+jifong@users.noreply.github.com>
2021-03-18 19:01:53 +0800

    Update Bubble Sort.cpp

--- 755227/Bubble Sort.cpp
+++ 755227/Bubble Sort.cpp
@@ -3,31 +3,7 @@
 using namespace std;
 
 int main() {
-    int num[4];
-    int space = 0;
-
-    for (int i = 0; i < 5; i++) {
-        cin >> num[i];
-    }
-
-    for (int i = 5; i > 0; i--) {
-        for (int j = 0; j < i; j++) {
-            if (num[j] > num[j + 1]) {
-                space = num[j];
-                num[j] = num[j + 1];
-                num[j + 1] = space;
-                space = 0;
-            }
-        }
-    }
-
-    for (int i = 0; i < 5; i++) {
-        cout << num[i] << " ";
-    }
-
-    cout << endl << endl;
-
-    int k[4];
+    int k[5];
     int spacee;
     srand(time(NULL));
     
